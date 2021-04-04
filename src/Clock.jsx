f330a53ab9789917dd1c96dcbc784df9d26dadd5
KaranKumarr <Karankumar0334@gmail.com>
2021-04-05 03:25:01 +0500

    V1 - stable

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,7 +1,24 @@
 import React, { useState } from "react";
+import './Clock.css';
 
 const Clock = () => {
-  return <></>
+
+  let time = new Date().toLocaleTimeString();
+
+  const [cTime, setCTime] = useState(time);
+
+  setInterval(function updatingTime() {
+    let newTime = new Date().toLocaleTimeString();
+    setCTime(newTime);
+  }, 1000);
+
+  return <>
+    <div className="content">
+      <div className="box">
+        <h1>{cTime}</h1>
+      </div>
+    </div>
+  </>
 };
 
 export default Clock;
